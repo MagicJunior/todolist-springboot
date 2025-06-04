c97bd1314e492f089ec1578d41d0593018d5413d
MagicJunior <junior.cobol@gmail.com>
2025-06-03 23:48:20 -0300

    Ajuste

---
+++ todolist/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tasks (titulo, descricao, concluida) VALUES 
+('Estudar Spring Boot', 'Praticar desenvolvimento com Spring Boot', false),
+('Fazer exercícios físicos', 'Ir à academia ou correr no parque', false),
+('Ler um livro', 'Ler pelo menos 30 páginas de um livro', true);
